3ea9a9477851aff98bf97a4f4dcfa7b93f2bc028
josephmangara <josephmangara2@gmail.com>
2023-12-12 15:10:32 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
 class Dog:
-    def __init__(self, name, age, breed="Mutt"):
-        self.name = f"name: {name}"
-        self.breed = f"breed: {breed}"
-        self.age = f"age: {age}"
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        # self.age = age
+        # self.name = f"name: {name}"
+        # self.breed = f"breed: {breed}"
+        # self.age = f"age: {age}"
 
     def adopt(self, owner_name):
         self.owner = owner_name
 
-snoopy = Dog("Snoppy", "3", "Rottweiler")
+snoopy = Dog("Snoppy", "Rottweiler")
 print(snoopy.breed)
-print(snoopy.age)
+# print(snoopy.age)
 
 # fido = Dog("Fido")
 # fido.adopt("Sophie")
