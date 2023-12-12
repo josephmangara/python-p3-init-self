a5a1370918d7db8839b91d4a6a169187a64d5fca
josephmangara <josephmangara2@gmail.com>
2023-12-12 15:08:36 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, age, breed="Mutt"):
+        self.name = f"name: {name}"
+        self.breed = f"breed: {breed}"
+        self.age = f"age: {age}"
+
+    def adopt(self, owner_name):
+        self.owner = owner_name
+
+snoopy = Dog("Snoppy", "3", "Rottweiler")
+print(snoopy.breed)
+print(snoopy.age)
+
+# fido = Dog("Fido")
+# fido.adopt("Sophie")
+# print(fido.owner)
+# print(fido is fido.showing_self())
